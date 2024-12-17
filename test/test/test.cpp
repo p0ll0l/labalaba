353d6dfad66f84c44b0c819089949a066e84976e
p0ll0l <151682106+p0ll0l@users.noreply.github.com>
2024-12-17 15:38:21 +0300

    первый конфл

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -13,7 +13,7 @@ int main() {
     std::cout << (is_leap_year(2024) ? "2024 год високосный\n" : "2024 год не високосный\n");
     std::cout << "Отформатированная дата: " << format_date(start) << std::endl;
 
-    std::cout << "Разница между датами: " << DateConverter::days_difference(start, end) << " дней\n";
+    std::cout << "Разница  между датами: " << DateConverter::days_difference(start, end) << " дней\n";
     std::cout << (DateConverter::is_leap_year(2024) ? "2024 год високосный\n" : "2024 год не високосный\n");
     std::cout << "Отформатированная дата: " << DateConverter::format_date(start) << std::endl;
 
@@ -22,9 +22,9 @@ int main() {
     std::string date2 = "2023-08-15";
     std::string date3 = "08-15-2023";
 
-    std::cout << "Standard format for " << date1 << ": " << DateConverter::convertToStandardFormat(date1) << std::endl;
-    std::cout << "Standard format for " << date2 << ": " << DateConverter::convertToStandardFormat(date2) << std::endl;
-    std::cout << "Standard format for " << date3 << ": " << DateConverter::convertToStandardFormat(date3) << std::endl;
+    std::cout << "Standard format for " << date1 << "::" << DateConverter::convertToStandardFormat(date1) << std::endl;
+    std::cout << "Standard format for " << date2 << "::" << DateConverter::convertToStandardFormat(date2) << std::endl;
+    std::cout << "Standard format for " << date3 << "::" << DateConverter::convertToStandardFormat(date3) << std::endl;
 
     return 0;
 }
